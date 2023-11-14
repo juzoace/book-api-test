1fbebbf4ad101fcd2f4e5d6f2f093d770f0fd317
juzoace <uzochukwunwigwe@gmail.com>
2023-11-14 05:32:59 +0100

    updated api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
 import { GraphQLAppModule } from './common/graphql.module'; 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [GraphQLAppModule, BookModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
