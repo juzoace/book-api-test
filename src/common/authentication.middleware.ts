b2e92fe3331282ae8f7bc330edd0fa90b7d792a7
juzoace <uzochukwunwigwe@gmail.com>
2023-11-15 10:44:59 +0100

    Updated fron end

--- src/common/authentication.middleware.ts
+++ src/common/authentication.middleware.ts
@@ -9,16 +9,16 @@ import { auth } from 'express-openid-connect';
 export class AuthenticationMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next) {
     console.log("here in auth middleware")
-    console.log(`${process.env.SECRET}`)
-    console.log(process.env.CLIENTID)
-    console.log(process.env.BASEURL)
-    console.log(process.env.iSSUERBASEURL)
+    // console.log(`${process.env.SECRET}`)
+    // console.log(process.env.CLIENTID)
+    // console.log(process.env.BASEURL)
+    // console.log(process.env.iSSUERBASEURL)
     auth({
         authRequired: true,
-        issuerBaseURL: `${process.env.iSSUERBASEURL}`,
-        baseURL: `${process.env.BASEURL}`,
-        clientID: `${process.env.CLIENTID}`,
-        secret: `${process.env.SECRET}`,
+        issuerBaseURL: `https://dev-st4zy53myhpbn3os.us.auth0.com`,
+        baseURL: `https://book-api-auth0-e4ad716ccb6a.herokuapp.com`,
+        clientID: `WQT5E0yE5LfqoZaHuJRYnojpJ8zy9GXb`,
+        secret: `N8deeMmR7Bc9_EMtZtStNAY-BvCI38FJWMSpK8Hw_UtWg4vFCwbdRbGIxRj2lwO7`,
         idpLogout: true,
       })
 
